36ad271f3a803bfa1971a29c4cf16be238a44e1a
David Farr <david.farr@sap.com>
2015-10-07 16:38:04 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,40 +1,91 @@
+'use strict';
 
+var module = angular.module('app', ['ui.bootstrap']);
 var format = require('string-format');
 var uuid = require('node-uuid');
 var exec = require('child_process').exec;
 
-var localGitRepos = [
-    {dir: '.', remote: 'origin', branch: 'fire/' + uuid.v4()}
-];
-
-var cmd = 'cd {0} && git add . && git commit -m "fire commit" && git push -f {1} HEAD:{2};';
-
-var firebase = new Firebase('wss://developer-api.nest.com');
-
-firebase.authWithCustomToken(process.env.TOKEN, function() {});
-
-firebase.once('value', function(snapshot) {
-
-    var alarms = snapshot.child('devices/smoke_co_alarms');
-
-    for(var id in alarms.val()) {
-
-        var alarm = alarms.child(id);
-
-        alarm.child('smoke_alarm_state').ref().on('value', function(state) {
-
-            console.log(state.val());
-
-            if(state.val() === 'emergency') {
-
-                // fire push each repo
-                localGitRepos.forEach(function(repo) {
-                    exec(format(cmd, repo.dir, repo.remote, repo.branch), function(err, stdout, stderr) {
-                        console.log(err, stdout, stderr);
-                    });
-                });
-            }
-
-        });
-    }
+angular.element(document).ready(function() {
+    angular.bootstrap(document, ['app']);
 });
+
+module.controller('AppCtrl', ['$scope', function($scope) {
+
+    $scope.state = 'ok';
+
+    $scope.repos = [];
+
+    try {
+        $scope.repos = JSON.parse(localStorage.repos) || [];
+    } catch(ex) {}
+
+    $scope.cmd = localStorage.cmd || 'cd {0} && git add . && git commit -m "fire commit" && git push -f {1} HEAD:{2}/{3}';
+
+    var firebase = new Firebase('wss://developer-api.nest.com');
+
+    firebase.authWithCustomToken(process.env.TOKEN, function() {});
+
+    firebase.once('value', function(snapshot) {
+
+        var alarms = snapshot.child('devices/smoke_co_alarms');
+
+        for(var id in alarms.val()) {
+
+            var alarm = alarms.child(id);
+
+            alarm.child('smoke_alarm_state').ref().on('value', function(state) {
+
+                $scope.$apply(function() {
+                    $scope.state = state.val();
+                });
+
+                if(state.val() === 'emergency') {
+
+                    // fire push each repo
+                    $scope.repos.forEach(function(repo) {
+                        exec(format($scope.cmd, repo.path, repo.remote, repo.branch, repo.uuid), function(err, stdout, stderr) {
+                            console.log(err, stdout, stderr);
+                            if(!err) {
+                                repo.uuid = uuid.v4();
+                            }
+                        });
+                    });
+                }
+            });
+        }
+    });
+
+    //
+    // Actions
+    //
+
+    $scope.add = {};
+
+    $scope.addRepo = function() {
+        $scope.repos.push({
+            path: $scope.add.path, 
+            remote: $scope.add.remote, 
+            branch: $scope.add.branch, 
+            uuid: uuid.v4()
+        });
+
+        $scope.add = {};
+    };
+
+    $scope.rmvRepo = function(i) {
+        $scope.repos.splice(i, 1);
+    };
+
+    //
+    // Watches
+    //
+
+    $scope.$watch('repos', function(repos) {
+        localStorage.repos = JSON.stringify(repos);
+    }, true);
+
+    $scope.$watch('cmd', function(cmd) {
+        localStorage.cmd = cmd;
+    });
+
+}]);
